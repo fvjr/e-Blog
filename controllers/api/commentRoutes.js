6c2e79548c7a61e70aaf61ccc82dbd21151b3977
Fernando Vasquez <vasquezjrf@gmail.com>
2022-03-11 13:52:02 -0500

    updated get, put, and delete routes for post and comments

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,6 +2,27 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+//http://localhost:3001/api/comments/
+router.get('/', async (req, res) => {
+  try {
+    const commentData = await Comment.findAll();
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// http://localhost:3001/api/comments/:id
+router.get('/:id', async (req, res) => {
+  try {
+    const commentData = await Comment.findByPk(req.params.id);
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//http://localhost:3001/api/comments/
 router.post('/', async (req, res) => {
   try {
     const newComment = await Comment.create({
@@ -14,15 +35,27 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.get('/', async (req, res) => {
+// http://localhost:3001/api/comments/:id
+router.put('/:id', async (req, res) => {
   try {
-    const commentData = await Comment.findAll();
-    res.status(200).json(commentData);
+    const updatedComment = await Comment.update(
+      {
+        content: req.body.content,
+        updatedAt: req.body.updatedAt,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(updatedComment);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+// http://localhost:3001/api/comments/:id
 router.delete('/:id', async (req, res) => {
   try {
     const commentData = await Comment.destroy({
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,15 +2,7 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
-  try {
-    const newPost = await Post.create(req.body);
-    res.status(200).json(newPost);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
+//http://localhost:3001/api/posts
 router.get('/', async (req, res) => {
   try {
     const postData = await Post.findAll();
@@ -20,12 +12,33 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
+//http://localhost:3001/api/posts/:id
+router.get('/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//http://localhost:3001/api/posts
+router.post('/', async (req, res) => {
+  try {
+    const newPost = await Post.create(req.body);
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//http://localhost:3001/api/posts
+router.delete('/:id', async (req, res) => {
   try {
     const postData = await Post.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
+        // user_id: req.session.user_id,
       },
     });
 
