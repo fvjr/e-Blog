3e9ce6ec3cdc898a153581cd7626e7a4b6414568
Fernando Vasquez <vasquezjrf@gmail.com>
2022-03-11 14:01:07 -0500

    added get all and get by id routes to user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,26 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+//http://localhost:3001/api/users/
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findAll();
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//http://localhost:3001/api/users/
+router.get('/:id', async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
